bcd8c0deaae2a56de296a035da12fa65e9f7aea3
Priyanka <kadam.priyanka1@gmail.com>
2019-11-22 12:38:02 +0530

    first commit

---
+++ coreDateDemo/SecondVcViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondVcViewController.swift
+//  coreDateDemo
+//
+//  Created by DebasishSen on 01/11/19.
+//  Copyright © 2019 DebasishSen. All rights reserved.
+//
+
+import UIKit
+
+class SecondVcViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
